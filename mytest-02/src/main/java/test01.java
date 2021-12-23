50a0db87514bd537a842202dcf970eeddae5e864
tkzou <tkzou@trip.com>
2021-12-23 19:39:51 +0800

    test

---
+++ mytest-02/src/main/java/test01.java
@@ -0,0 +1,14 @@
+/**
+ * @Author: tk.zou
+ * @Description:
+ * @Date: 2021-12-23 19:37
+ * @Version: 1.0.0
+ */
+
+
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello,trip");
+        System.out.println("hello");
+    }
+}
