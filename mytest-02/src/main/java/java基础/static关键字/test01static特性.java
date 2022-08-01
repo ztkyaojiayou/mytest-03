bf76901b4eb6f7114da44dfbecc51ad7ffc4c985
tkzou <tongkunzou0807@163.com>
2022-08-01 16:57:13 +0800

    commit

--- mytest-02/src/main/java/java基础/static关键字/test01static特性.java
+++
@@ -1,9 +0,0 @@
-package java基础.static关键字;
-
-public class test01static特性 {
-public  static int x = 9;
-//public  int x = 9;
-public static void main(String[] args){
-            System.out.println("Value is " +x);
-            }
-}
--- mytest-02/src/main/java/秋招笔试/京东/test01.java
+++
@@ -1,9 +0,0 @@
-package 秋招笔试.京东;
-
-public class test01 {
-    public static void main(String[] args) {
-int[][] A =new int[][]{{1,2},{4,5},{7,8,9,10,11,12},{}};
-        System.out.println(A.length);
-        System.out.println(A[0].length);
-    }
-}
